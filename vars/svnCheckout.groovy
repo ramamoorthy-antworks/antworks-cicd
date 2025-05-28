3bcbee246ce8ae189a83bd0c2a5cb96c45364ec1
Ram <ramamoorthy.p@ant.works>
2025-05-28 13:15:50 +0530

    Update svnCheckout.groovy

--- vars/svnCheckout.groovy
+++ vars/svnCheckout.groovy
@@ -1,23 +1,6 @@
-def call(String repoUrl, String credentialsId='Ram' ) {
-    checkout([
-        $class: 'SubversionSCM',
-        additionalCredentials: [],
-        excludedCommitMessages: '',
-        excludedRegions: '',
-        excludedRevprop: '',
-        excludedUsers: '',
-        filterChangelog: false,
-        ignoreDirPropChanges: false,
-        includedRegions: '',
-        locations: [[
-            cancelProcessOnExternalsFail: true,
-            credentialsId: credentialsId,
-            depthOption: 'infinity',
-            ignoreExternalsOption: true,
-            local: '.', 
-            remote: repoUrl
-        ]],
-        quietOperation: true,
-        workspaceUpdater: [$class: 'CheckoutUpdater']
-    ])
+def svnCheckout(String repoUrl, String credentialsId) {
+    checkout([$class: 'SubversionSCM',
+              locations: [[credentialsId: credentialsId,
+                           remote: repoUrl]],
+              workspaceUpdater: [$class: 'UpdateUpdater']])
 }
