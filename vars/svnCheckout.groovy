03dd3e6e6e451ff12433350e179640328a5c4222
Ram <ramamoorthy.p@ant.works>
2025-05-28 13:19:27 +0530

    Update svnCheckout.groovy

--- vars/svnCheckout.groovy
+++ vars/svnCheckout.groovy
@@ -1,6 +1,6 @@
 def svnCheckout(String repoUrl, String credentialsId) {
     checkout([$class: 'SubversionSCM',
-              locations: [[credentialsId: credentialsId,
-                           remote: repoUrl]],
+              locations: [[credentialsId: 'Ram',
+                           remote: 'https://awchnsvr05.ant.works/svn/CMRPlus/Platform_React_Core/branches/CMR_API_Core']],
               workspaceUpdater: [$class: 'UpdateUpdater']])
 }
